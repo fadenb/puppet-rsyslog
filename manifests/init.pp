7b01fcd02dd266d637aa285c463ce5875c03eb78
Tristan Helmich <tristan.helmich@mayflower.de>
2013-06-25 11:27:06 +0200

    added anchors to init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -31,7 +31,9 @@ class rsyslog (
   $client_conf            = $rsyslog::params::client_conf,
   $server_conf            = $rsyslog::params::server_conf,
 ) inherits rsyslog::params {
-  class { 'rsyslog::install': }
-  class { 'rsyslog::config': }
-  class { 'rsyslog::service': }
+  anchor { 'rsyslog::begin': }  ->
+  class { 'rsyslog::install': } ->
+  class { 'rsyslog::config': }  ->
+  class { 'rsyslog::service': } ->
+  anchor { 'rsyslog::end': }
 }
